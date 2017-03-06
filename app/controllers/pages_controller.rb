d89c497075ec77b11cb367f3eebdfd147457e3da
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2017-03-06 13:27:10 +0100

    pagina progetto fatta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :project ]
 
   def home
+  end
+
+  def project
   end
 end
