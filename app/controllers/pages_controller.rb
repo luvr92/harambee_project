785a0e428faa19b045dd20dac587b06270b98457
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2017-03-07 13:41:19 +0100

    template ultimato

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,24 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :project ]
+  skip_before_action :authenticate_user!, only: [ :home, :project, :edu, :culture, :agriculture, :social, :sport ]
 
   def home
   end
 
   def project
+  end
+
+  def edu
+  end
+
+  def culture
+  end
+
+  def agriculture
+  end
+
+  def social
+  end
+
+  def sport
   end
 end
