ef248f161923411eefa4e984efefdf325f4e45a6
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2017-03-27 16:43:55 +0200

    pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'agriculture', to: 'pages#agriculture'
   get 'social', to: 'pages#social'
   get 'sport', to: 'pages#sport'
+  get 'staff', to: 'pages#staff'
   resources :interns, only: [:new, :create]
   resources :volunteers, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
