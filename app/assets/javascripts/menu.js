244796a623a921155fd2c3ffd868a31c2f507eab
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2017-04-11 12:35:55 +0200

    nother tiny fix

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
   $('#who-we-are').on('click', function(event) {
+    $('#news-menu').hide();
+    $('#jobapp-menu').hide();
+    $('#activities-menu').hide();
     $('#who-we-are-menu').toggle();
   });
   $('#activities').on('click', function(event) {
+    $('#who-we-are-menu').hide();
+    $('#news-menu').hide();
+    $('#jobapp-menu').hide();
     $('#activities-menu').toggle();
   });
   $('#jobapp').on('click', function(event) {
+    $('#who-we-are-menu').hide();
+    $('#news-menu').hide();
+    $('#activities-menu').hide();
     $('#jobapp-menu').toggle();
   });
   $('#news').on('click', function(event) {
+    $('#jobapp-menu').hide();
+    $('#activities-menu').hide();
+    $('#who-we-are-menu').hide();
     $('#news-menu').toggle();
   });
 });
